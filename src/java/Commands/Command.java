d2aa2c4479909d482592f674e4e3af076bd5b898
Lauren Delaney <ren.delaney@gmail.com>
2016-11-24 14:18:21 +0000

    added command pattern

---
+++ src/java/Commands/Command.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Commands;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author D00165309
+ */
+public interface Command {
+
+    public String execute(HttpServletRequest request, HttpServletResponse response);
+
+}
