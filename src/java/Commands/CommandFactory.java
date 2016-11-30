8dc4c83a6094a11beae530ff1c54cdc2aaffa67c
D00165309 <D00165309@P1158-PC19.ad.dkit.ie>
2016-11-30 23:04:02 +0000

    Added Command Factory file (no commands yet)

---
+++ src/java/Commands/CommandFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Commands;
+
+/**
+ *
+ * @author Ren
+ */
+public class CommandFactory {
+    public Command createCommand(String action){
+        Command cmd = null;
+        if(action.equals("")){
+           // cmd = new commandName();
+        }
+        return null;
+    }
+}
