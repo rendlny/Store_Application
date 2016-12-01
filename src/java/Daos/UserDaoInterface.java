7f451ea235f7faca56caa0161c9207f617cc6117
Lauren Delaney <ren.delaney@gmail.com>
2016-12-01 18:49:53 +0000

    userDaoInterface

---
+++ src/java/Daos/UserDaoInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DTO.User;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author d00167267
+ */
+public interface UserDaoInterface {
+
+    public ArrayList<User> getAllUsers();
+    public User checkLogin(String username, String password);
+    public int addUser(User u);
+    public User getUserByUsername(String username);
+    public int deleteUser(String username);
+}
