6e39a79e69f4a461485954dc93bef339e4e9ee6b
rendlny <ren.delaney@gmail.com>
2016-12-01 13:53:42 +0000

    DAO and ProductDaoInterface

---
+++ src/java/Daos/ProductDaoInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Daos;
+
+import Dtos.Product;
+import Dtos.ProductImage;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ren
+ */
+public interface ProductDaoInterface {
+    public boolean addProduct(Product p);
+    public boolean deleteProduct(Product p);
+    public ArrayList<Product> listAllProducts();
+    public ArrayList<Product> searchProducts(String searchTerm);
+    public ArrayList<Product> addImageToProduct(ProductImage img);
+    public ArrayList<Product> removeImageFromProduct(ProductImage img);
+    
+}
